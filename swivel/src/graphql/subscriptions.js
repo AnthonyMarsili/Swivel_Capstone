4542bbfb1552fa31011f78840ffae6c813976530
AnthonyMarsili <56132966+AnthonyMarsili@users.noreply.github.com>
2021-03-23 19:05:20 -0400

    created api

--- swivel/src/graphql/subscriptions.js
+++ swivel/src/graphql/subscriptions.js
@@ -1,35 +1,167 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const onCreateTodo = /* GraphQL */ `
-  subscription OnCreateTodo {
-    onCreateTodo {
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser {
+    onCreateUser {
       id
-      name
-      description
+      student
+      first_name
+      last_name
       createdAt
+      password
+      values
+      benefits
+      tech_skills
+      soft_skills
+      employment_type
+      about
+      picture
+      availability
+      notification_settings
+      preferences
+      response_rate
+      matches
+      email
+      organization
+      position
+      company_size
+      location
+      website
+      alumn
+      life_at
+      degree
+      major
+      located
+      grad_year
+      highlights
+      special
+      resume
+      bio
       updatedAt
     }
   }
 `;
-export const onUpdateTodo = /* GraphQL */ `
-  subscription OnUpdateTodo {
-    onUpdateTodo {
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser {
+    onUpdateUser {
       id
-      name
-      description
+      student
+      first_name
+      last_name
       createdAt
+      password
+      values
+      benefits
+      tech_skills
+      soft_skills
+      employment_type
+      about
+      picture
+      availability
+      notification_settings
+      preferences
+      response_rate
+      matches
+      email
+      organization
+      position
+      company_size
+      location
+      website
+      alumn
+      life_at
+      degree
+      major
+      located
+      grad_year
+      highlights
+      special
+      resume
+      bio
       updatedAt
     }
   }
 `;
-export const onDeleteTodo = /* GraphQL */ `
-  subscription OnDeleteTodo {
-    onDeleteTodo {
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser {
+    onDeleteUser {
       id
-      name
-      description
+      student
+      first_name
+      last_name
       createdAt
+      password
+      values
+      benefits
+      tech_skills
+      soft_skills
+      employment_type
+      about
+      picture
+      availability
+      notification_settings
+      preferences
+      response_rate
+      matches
+      email
+      organization
+      position
+      company_size
+      location
+      website
+      alumn
+      life_at
+      degree
+      major
+      located
+      grad_year
+      highlights
+      special
+      resume
+      bio
+      updatedAt
+    }
+  }
+`;
+export const onCreateMatch = /* GraphQL */ `
+  subscription OnCreateMatch {
+    onCreateMatch {
+      id
+      company
+      student
+      createdAt
+      status_flag
+      upcoming_meeting
+      messages
+      updatedAt
+    }
+  }
+`;
+export const onUpdateMatch = /* GraphQL */ `
+  subscription OnUpdateMatch {
+    onUpdateMatch {
+      id
+      company
+      student
+      createdAt
+      status_flag
+      upcoming_meeting
+      messages
+      updatedAt
+    }
+  }
+`;
+export const onDeleteMatch = /* GraphQL */ `
+  subscription OnDeleteMatch {
+    onDeleteMatch {
+      id
+      company
+      student
+      createdAt
+      status_flag
+      upcoming_meeting
+      messages
       updatedAt
     }
   }
