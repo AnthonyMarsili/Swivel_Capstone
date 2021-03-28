0252c1fde705f78a81a41cb162093f11509236ef
Emma Landry <emmkatlan123@gmail.com>
2021-03-28 18:33:55 -0400

    dashboard tabs and search

--- swivel/src/components/Dashboard.js
+++ swivel/src/components/Dashboard.js
@@ -1,12 +1,14 @@
+import MainNav from "./MainNav"
+import SideNav from "./SideNav"
+import Tabs from "./Tabs"
+import Search from "./Search"
+
 import React, { useState, useEffect } from 'react'
 import Amplify, { API, Auth, Hub } from 'aws-amplify'
 import {Link, Redirect} from 'react-router-dom'
 
-import MainNav from './MainNav'
-import SideNav from './SideNav'
-
 const Dashboard = ( ) => {
-  const [redirectToLogin, setRedirectToLogin] = useState(false)
+    const [redirectToLogin, setRedirectToLogin] = useState(false)
 
   useEffect(() => {
     Auth.currentAuthenticatedUser()
@@ -23,13 +25,27 @@ const Dashboard = ( ) => {
     }
     {
       redirectToLogin === false && (
-        <div>
-          <div>
-            <MainNav />
-            <SideNav />
-          </div>
-          <div className = "dashboard-body">
-            <div className="connections-div">
+        <div className = "dashboard-body">
+        <MainNav />
+        <SideNav />
+          <div className="connections-div">
+            <p className="dashboard-title"> Connections </p>
+            <div className="connections-elem">
+              <Tabs>
+                <div label="Upcoming">
+                  <div className="tab-content"><p> Upcoming </p></div>
+                </div>
+                <div label="Pending">
+                  <div className="tab-content"><p> Pending </p></div>
+                </div>
+                <div label="Active">
+                  <div className="tab-content"><p> Active </p></div>
+                </div>
+                <div label="Archived">
+                  <div className="tab-content"><p> Archived </p></div>
+                </div>
+              </Tabs>
+              <Search />
             </div>
           </div>
         </div>
