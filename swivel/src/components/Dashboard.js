ec402a401c7b7321ca254c8649e56c688193a1eb
Emma Landry <emmkatlan123@gmail.com>
2021-03-28 22:08:43 -0400

    querying for matches, displaying message when no matches

--- swivel/src/components/Dashboard.js
+++ swivel/src/components/Dashboard.js
@@ -2,19 +2,48 @@ import MainNav from "./MainNav"
 import SideNav from "./SideNav"
 import Tabs from "./Tabs"
 import Search from "./Search"
+import NoMatches from "./NoMatches"
 
 import React, { useState, useEffect } from 'react'
 import Amplify, { API, Auth, Hub } from 'aws-amplify'
 import {Link, Redirect} from 'react-router-dom'
+import { listMatchs } from '../graphql/queries'
+
+const matchList = {}
 
 const Dashboard = ( ) => {
-    const [redirectToLogin, setRedirectToLogin] = useState(false)
+  const [redirectToLogin, setRedirectToLogin] = useState(false)
+  const [matches, setMatches] = useState(matchList);
 
   useEffect(() => {
-    Auth.currentAuthenticatedUser()
-      .then(() => { return true; })
-      .catch(() => { setRedirectToLogin(true) })
+    let isCancelled = false
+    Auth.currentSession()
+      .then(currUser => {
+        var userID = currUser['idToken']['payload']['sub']
+        getMatches(userID)
+        return () => {
+          isCancelled = true
+        }
+       })
+      .catch(err => {
+        setRedirectToLogin(true)
+      })
   })
+
+
+  async function getMatches(userID) {
+    await API.graphql({ query: listMatchs, variables: {id: userID.toString() }}).then(response => {
+      var matches = response.data.listMatchs.items;
+      if(matches.length == 0){
+        setMatches(() => ({ ...matches, matches: false}))
+      }
+      else {
+        setMatches(() => ({ ...matches, matches: response}))
+      }
+    }).catch(err => {
+      console.log(err);
+    });
+  }
 
   return (
     <div className = "dashboard">
@@ -31,20 +60,24 @@ const Dashboard = ( ) => {
           <div className="connections-div">
             <p className="dashboard-title"> Connections </p>
             <div className="connections-elem">
+            {
+              matches.matches === false && (
               <Tabs>
                 <div label="Upcoming">
-                  <div className="tab-content"><p> Upcoming </p></div>
+                  <NoMatches title="upcoming" />
                 </div>
                 <div label="Pending">
-                  <div className="tab-content"><p> Pending </p></div>
+                  <NoMatches title="pending" />
                 </div>
                 <div label="Active">
-                  <div className="tab-content"><p> Active </p></div>
+                  <NoMatches title="active" />
                 </div>
                 <div label="Archived">
-                  <div className="tab-content"><p> Archived </p></div>
+                  <NoMatches title="archived" />
                 </div>
               </Tabs>
+            )
+            }
               <Search />
             </div>
           </div>
---
+++ swivel/src/components/NoMatches.js
@@ -0,0 +1,9 @@
+const NoMatches = ({ title }) => {
+  return (
+    <div className="tab-content">
+      <p> You have no {title} matches </p>
+    </div>
+  )
+}
+
+export default NoMatches
